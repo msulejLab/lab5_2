647263c67f2f7d38e17623b33eeb46a0bc930c92
Radek Adamus <adamus.r@gmail.com>
2015-04-26 13:01:17 +0200

    ApplicationMain - uncomment lines that can be compiled withuot errors

--- src/main/java/edu/iis/mto/integrationtest/main/ApplicationMain.java
+++ src/main/java/edu/iis/mto/integrationtest/main/ApplicationMain.java
@@ -1,15 +1,22 @@
 package edu.iis.mto.integrationtest.main;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.iis.mto.integrationtest.utils.Mode;
+import edu.iis.mto.integrationtest.utils.ModeUtils;
+
 
 public class ApplicationMain {
 
 //    private static ApplicationContext applicationContext;
 //
-//    private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationMain.class);
-//
-//    public static void main(String[] args) {
-//        /* Ustawienie trybu dzia³ania aplikacji */
-//        ModeUtils.setMode(Mode.DEV);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationMain.class);
+
+    public static void main(String[] args) {
+        /* Ustawienie trybu dzia³ania aplikacji */
+        ModeUtils.setMode(Mode.DEV);
+        LOGGER.debug("Application mode set to ", Mode.DEV);
 //
 //        /* Konfiguracja kontekstu Spring i pobranie z kontenera bean'a reprezentujacego repozytorium danych*/
 //        applicationContext = new AnnotationConfigApplicationContext(ApplicationConfig.class);
@@ -23,5 +30,5 @@ public class ApplicationMain {
 //        /* Pobranie liczby rekordów w repozytorium */
 //        long recordsNumber = repository.count();
 //        LOGGER.debug("Found {} records in total", recordsNumber);
-//    }
+    }
 }
